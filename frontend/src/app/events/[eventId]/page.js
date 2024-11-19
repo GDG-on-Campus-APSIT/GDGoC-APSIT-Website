3aa950936f001aab6745eab2be5657fa7f3ba27e
GouravMidya <gouravgmidya@gmail.com>
2024-11-19 13:08:54 +0530

    minor changes

--- frontend/src/app/events/[eventId]/page.js
+++ frontend/src/app/events/[eventId]/page.js
@@ -4,7 +4,7 @@ import EventDetails from '@/components/eventdetails'
 import { useParams } from 'next/navigation';
 import { NavbarComponent } from '@/components/navbar';
 
-export default function eventDetailsPage() {
+export default function EventDetailsPage() {
     const { eventId } = useParams();
 
     return (
